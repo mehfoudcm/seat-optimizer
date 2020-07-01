9a9bb64d8c276b3c670fa117114cead9e93e44e0
mehfoudcm <mehfoudcm@vcu.edu>
2020-07-01 08:23:13 -0400

    Update distance_fun.py

--- distance_fun.py
+++ distance_fun.py
@@ -22,11 +22,11 @@ possible for the function to hit errors if any of the coordinates are not intege
 def distance_fun(clump_df,i,j,top_end_threshold):
     dist_list = []
     for w in clump_df.x_coords[i]:
-        for x in clump_df.y_coords[i]:
-            for y in clump_df.x_coords[j]:
-                for z in clump_df.y_coords[j]:
+		for x in clump_df.y_coords[i]:
+			for y in clump_df.x_coords[j]:
+				for z in clump_df.y_coords[j]:
 					dist_val = np.sqrt((y-w)**2+(z-x)**2)
 					dist_list.append(dist_val)
 					if dist_val > top_end_threshold*1.5:
 						return dist_val
-    return np.min(dist_list)
+	return np.min(dist_list)
