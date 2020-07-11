fb843c99122fd7b79f018ff346f1b57b20bad56b
mehfoudcm <mehfoudcm@vcu.edu>
2020-07-11 15:47:20 -0400

    made adjustments to check seats to make sure that it works for the right files in the right directory

--- check_seats.py
+++ check_seats.py
@@ -28,34 +28,36 @@ def check_seats():
 
     # goes through the folder of optimized seats, to perform analysis on each group
     for spec_group in listdir(optimized_seats):
-        group_label.append(spec_group.replace('group_label_','').replace('_opt_seats.csv',''))
+        if 'group_label_' in spec_group:
+            print(spec_group.replace('group_label_','').replace('_opt_seats.csv',''))
+            group_label.append(spec_group.replace('group_label_','').replace('_opt_seats.csv',''))
     
     
-        # reads the specific optimized seats file in and reduces it to only clusters indicated
-        df_opt_seats = pd.read_csv(optimized_seats+spec_group)
-        df_opt_seats = df_opt_seats[df_opt_seats.clump_ind == 1]
-        df_opt_seats = df_opt_seats.reset_index()
-        # sets coordinates to be literal list read in
-        for i in range(len(df_opt_seats)):
-            df_opt_seats.x_coords[i] = ast.literal_eval(df_opt_seats.x_coords[i])
-            df_opt_seats.y_coords[i] = ast.literal_eval(df_opt_seats.y_coords[i])
+            # reads the specific optimized seats file in and reduces it to only clusters indicated
+            df_opt_seats = pd.read_csv(optimized_seats+spec_group)
+            df_opt_seats = df_opt_seats[df_opt_seats.clump_ind == 1]
+            df_opt_seats = df_opt_seats.reset_index()
+            # sets coordinates to be literal list read in
+            for i in range(len(df_opt_seats)):
+                df_opt_seats.x_coords[i] = ast.literal_eval(df_opt_seats.x_coords[i])
+                df_opt_seats.y_coords[i] = ast.literal_eval(df_opt_seats.y_coords[i])
+            
+            # reads in the distance from each cluster (A) and then uses the distance function
+            # to calculate the min distance of every other cluster (B, C, D...) from that initial cluster (A)
+            df_opt_seats['min_dist'] = [0]*len(df_opt_seats)
+            for ind_seat in range(len(df_opt_seats)):
+                seat_distances = []
+                for other_seat in range(len(df_opt_seats)):
+                    if other_seat != ind_seat:
+                        seat_distances.append(distance_fun(df_opt_seats, ind_seat, other_seat, top_end_threshold))
+            
+                df_opt_seats.min_dist[ind_seat] = min(seat_distances)
         
-        # reads in the distance from each cluster (A) and then uses the distance function 
-        # to calculate the min distance of every other cluster (B, C, D...) from that initial cluster (A)
-        df_opt_seats['min_dist'] = [0]*len(df_opt_seats)
-        for ind_seat in range(len(df_opt_seats)):
-            seat_distances = []
-            for other_seat in range(len(df_opt_seats)):
-                if other_seat != ind_seat:
-                    seat_distances.append(distance_fun(df_opt_seats, ind_seat, other_seat, top_end_threshold))
+            # sets up aggregate for calculating measures of that minimum distance of the specific clusters
+            overall_min_distance.append(min(df_opt_seats.min_dist))
+            overall_max_distance.append(max(df_opt_seats.min_dist))
+            overall_avg_min_dist.append(round(sum(df_opt_seats.min_dist)/len(df_opt_seats.min_dist),2))
         
-            df_opt_seats.min_dist[ind_seat] = min(seat_distances)
-    
-        # sets up aggregate for calculating measures of that minimum distance of the specific clusters
-        overall_min_distance.append(min(df_opt_seats.min_dist))
-        overall_max_distance.append(max(df_opt_seats.min_dist))
-        overall_avg_min_dist.append(round(sum(df_opt_seats.min_dist)/len(df_opt_seats.min_dist),2))
-    
     # puts the list together to create a dataframe
     df_lists = list(zip(group_label, overall_min_distance, overall_max_distance, overall_avg_min_dist))
 
